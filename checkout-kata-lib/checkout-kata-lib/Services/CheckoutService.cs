7b3036db33adc0bf4ee350b45d3acd35069ae748
Alex Tan <tancl.alex@gmail.com>
2023-08-29 00:13:01 +0100

    prepare test data for scan

--- checkout-kata-lib/checkout-kata-lib/Services/CheckoutService.cs
+++ checkout-kata-lib/checkout-kata-lib/Services/CheckoutService.cs
@@ -2,6 +2,17 @@
 
 public class CheckoutService : ICheckout
 {
+    private readonly IEnumerable<StockKeepingUnit> stockKeepingUnits;
+    private readonly IEnumerable<Offer> offers;
+
+    private readonly IDictionary<string, int> unitQuantity;
+
+    public CheckoutService(IEnumerable<StockKeepingUnit> stockKeepingUnits, IEnumerable<Offer> offers)
+    {
+        this.stockKeepingUnits = stockKeepingUnits;
+        this.offers = offers;
+    }
+
     public int GetTotalPrice()
     {
         throw new NotImplementedException();
--- checkout-kata-lib/checkout-kata-test/CheckoutServiceTests.cs
+++ checkout-kata-lib/checkout-kata-test/CheckoutServiceTests.cs
@@ -2,11 +2,18 @@ using checkout_kata_lib;
 
 namespace checkout_kata_test;
 
-public class Tests
+public class CheckoutServiceTests
 {
+    private List<StockKeepingUnit> units = new List<StockKeepingUnit>();
+    private List<Offer> offers = new List<Offer>();
+
     [SetUp]
     public void Setup()
     {
+        units.Add(new StockKeepingUnit{ Identifier = "A", UnitPrice = 50 });
+        units.Add(new StockKeepingUnit{ Identifier = "B", UnitPrice = 30 });
+        units.Add(new StockKeepingUnit{ Identifier = "C", UnitPrice = 20 });
+        units.Add(new StockKeepingUnit{ Identifier = "D", UnitPrice = 10 });
     }
 
     [Test]
@@ -14,7 +21,7 @@ public class Tests
     {
         //Assemble
         var sku = "A";
-        var service = new CheckoutService();
+        var service = new CheckoutService(units, offers);
 
         //Act
         service.Scan(sku);
